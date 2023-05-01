d89abafced2a976b9bd1dbcb600bca242106c4b9
stlnss <stlnesslv@gmail.com>
2023-05-01 22:52:38 +1000

    Add ViewModel class  [Codelab 5/12]

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Unscramble
+# Unscramble Game
+
+# Reference
+
+- [ViewModel and State in Compose](https://developer.android.com/codelabs/basic-android-kotlin-compose-viewmodel-and-state)
---
+++ app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.android.unscramble.ui
+
+import androidx.lifecycle.ViewModel
+import com.example.android.unscramble.data.allWords
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class GameViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(GameUiState())
+    var uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf()
+
+    init {
+        resetGame()
+    }
+
+    private fun pickRandomWordAndShuffle(): String {
+        currentWord = allWords.random()
+        return if (usedWords.contains(currentWord)) {
+            pickRandomWordAndShuffle()
+        } else {
+            shuffleCurrentWord(currentWord)
+        }
+    }
+
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWords = word.toCharArray()
+        while (String(tempWords) == word) {
+            tempWords.shuffle()
+        }
+        return String(tempWords)
+    }
+
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
+    }
+
+}
